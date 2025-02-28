687f4225d839638945ffd6682fa68ade5d958fbc
MattCorwinBW <matt.corwin@os.brightway.com>
2025-02-28 10:40:19 -0600

    bugfix

--- src/client.ts
+++ src/client.ts
@@ -1,43 +1,56 @@
 const baseUrl = process.env.FLIP_API_BASE_URL ?? 'https://api.feature-flipper.com';
 
-const client = async ({ apiKey, tenantId }: { apiKey?: string; tenantId: string }) => {
-  let booleanFlippers = new Map<string, boolean>();
-  let stringListFlippers = new Map<string, string[]>();
+interface Flipper {
+  id: string;
+  name: string;
+  tenantId: string;
+  stage: string;
+  type: 'boolean' | 'stringList';
+  enabled?: boolean;
+  values?: string[];
+  isDefault: boolean;
+}
+
+const client = async ({ apiKey }: { apiKey?: string }) => {
+  let booleanFlippers = new Map<string, Flipper>();
+  let stringListFlippers = new Map<string, Flipper>();
+
   const key = apiKey || process.env.FLIP_API_KEY;
   if (!key || typeof key !== 'string') {
-    throw new Error('process.env.FLIP_API_KEY is not set, and no apiKey was provided');
+    throw new Error('process.env.FLIP_API_KEY is not set, and no API Key was provided');
   }
 
   const headers = {
     'x-api-key': key,
   };
 
-  const init = async () => {
+  const init = async ({ tenantId }: { tenantId: string }): Promise<Flipper[]> => {
     const encodedTenantId = encodeURIComponent(tenantId);
     const response = await fetch(`${baseUrl}/flippers/${encodedTenantId}`, { headers });
     const data = await response.json();
     if (data.length > 0) {
       data.forEach((flipper: any) => {
         if (flipper.type === 'boolean') {
-          booleanFlippers.set(flipper.name, flipper.value);
+          booleanFlippers.set(flipper.name, flipper);
         } else if (flipper.type === 'stringList') {
-          stringListFlippers.set(flipper.name, flipper.value);
+          stringListFlippers.set(flipper.name, flipper);
         }
       });
+      return data;
     }
+    return [];
   };
 
-  await init();
-
   const isEnabled = (flipperName: string) => {
-    return booleanFlippers.get(flipperName) ?? false;
+    return booleanFlippers.get(flipperName)?.enabled ?? false;
   };
 
   const getStringList = (flipperName: string) => {
-    return stringListFlippers.get(flipperName) ?? [];
+    return stringListFlippers.get(flipperName)?.values ?? [];
   };
 
   return {
+    init,
     isEnabled,
     getStringList,
   };
